7e950116edb71d7e3dbf2b8f7f4c125dede8751a
Tiago Guizelini <tguizelini@macbook-pro.local>
2020-03-12 05:03:50 -0300

    enabled eureka client

--- fornecedor/src/main/java/br/com/ht7/fornecedor/FornecedorApplication.java
+++ fornecedor/src/main/java/br/com/ht7/fornecedor/FornecedorApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FornecedorApplication {
 
 	public static void main(String[] args) {
--- loja/src/main/java/br/com/ht7/loja/LojaApplication.java
+++ loja/src/main/java/br/com/ht7/loja/LojaApplication.java
@@ -3,10 +3,12 @@ package br.com.ht7.loja;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class LojaApplication {
 
 	public static void main(String[] args) {
