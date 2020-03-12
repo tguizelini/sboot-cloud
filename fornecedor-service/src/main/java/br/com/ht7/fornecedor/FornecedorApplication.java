d141390af8535e2111070710b525c5d16441003b
Tiago Guizelini <tguizelini@macbook-pro.local>
2020-03-12 14:36:04 -0300

    troca dependencia do config client no fornecedor.. a starter-config-client, da 401

--- fornecedor-service/src/main/java/br/com/ht7/fornecedor/FornecedorApplication.java
+++ fornecedor-service/src/main/java/br/com/ht7/fornecedor/FornecedorApplication.java
@@ -2,6 +2,7 @@ package br.com.ht7.fornecedor;
 
 import br.com.ht7.fornecedor.documents.Fornecedor;
 import br.com.ht7.fornecedor.repositories.FornecedorRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +16,7 @@ import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaClient
+@Slf4j
 public class FornecedorApplication {
 
 	public static void main(String[] args) {
@@ -25,7 +27,7 @@ public class FornecedorApplication {
 	CommandLineRunner seed(FornecedorRepository repository) {
 		return args -> {
 			repository.deleteAll();
-			System.out.println("Fornecedores excluídos");
+			log.info("Fornecedores excluídos");
 
 			List<Fornecedor> fornecedores = new ArrayList<>();
 			fornecedores.add(new Fornecedor(null, "Ambev", "GO", "Rua da Maria"));
@@ -34,7 +36,7 @@ public class FornecedorApplication {
 			fornecedores.add(new Fornecedor(null, "Heineken", "SP", "Rua da breja"));
 
 			repository.saveAll(fornecedores);
-			System.out.println("Fornecedores incluídos");
+			log.info("Fornecedores incluídos");
 		};
 	}
 
