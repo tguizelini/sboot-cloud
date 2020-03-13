8f4f0e25f60c31cbc3398805f2135ef39ae78100
Tiago Guizelini <tguizelini@macbook-pro.local>
2020-03-13 12:07:36 -0300

    auth-server - config security e cloud oauth2

---
+++ auth-server/src/main/java/br/com/ht7/auth/configs/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package br.com.ht7.auth.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    //quem sabe fazer a autenticação
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return super.userDetailsService();
+    }
+
+    //usado para criptografar a senha
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ auth-server/src/test/java/br/com/ht7/auth/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ht7.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
